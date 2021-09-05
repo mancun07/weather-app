740ce0aed3d5dc8c1c7fa86ca4755b5cba9b7bc3
Dima <address>
2021-09-05 13:45:44 +0300

    a new commit

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import classes from './Loading.module.css'
+// import React from 'react'
+// import classes from './Loading.module.css'
 
-const Loading = () => {
-    return (
-<div classes={classes[`lds-ring`]}><div></div><div></div><div></div><div></div></div>
-    )
-}
+// const Loading = () => {
+//     return (
+// <div classes={classes[`lds-ring`]}><div></div><div></div><div></div><div></div></div>
+//     )
+// }
 
-export default Loading
+// export default Loading
